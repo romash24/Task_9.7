672a779ec93d3503c7bd01c7b9b7298dfc994daa
Roman Sigarev <roma2406@ya.ru>
2021-06-01 01:19:14 +0300

    fix error task 5

--- task5/script.js
+++ task5/script.js
@@ -2,15 +2,15 @@ const inputField = document.querySelector('input');
 const duplicateField = document.querySelector('#duplicateField');
 const pressButton = document.querySelector('button');
 
-inputField.addEventListener ('keypress', function(e) {
-    if (duplicateField.textContent == '') {
+inputField.addEventListener('keypress', function (e) {
+    if (inputField == '') {
         duplicateField.textContent = e.key;
     } else {
-        duplicateField.textContent = duplicateField.textContent + e.key;
-    }    
+        duplicateField.textContent = inputField.value + e.key;
+    }
 })
 
-pressButton.addEventListener ('click', function() {
+pressButton.addEventListener('click', function () {
     console.log(inputField.value);
     inputField.value = '';
     duplicateField.textContent = '';
